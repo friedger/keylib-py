3ce19321a3aa22bb39ed96c9d837ae3943c299c8
Dillon Enge <dillon.enge@gmail.com>
2018-02-27 20:47:12 -0500

    Incremented version to not be overridden.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keylib',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/blockstack/keylib-py',
     license='MIT',
     author='Blockstack Developers',
